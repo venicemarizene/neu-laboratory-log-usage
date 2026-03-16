955c01a99db8eb787f97a4db3c68d1e10d5c225c
Linga, Venice Marizene G. <venicemarizene.linga@neu.edu.ph>
2026-03-16 17:26:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8388808818-86dc2",
+  "appId": "1:875039772295:web:61a2ae6ebd8b8d2a1eb222",
+  "apiKey": "AIzaSyCNjmVo4bdBXusSLfGzZldy96j6XbR_YoQ",
+  "authDomain": "studio-8388808818-86dc2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "875039772295"
+};
